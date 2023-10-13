4e5a361eb0fc2dedc96921935b843a5b6785b21d
Jean <jeanttalar@gmail.com>
2023-10-13 13:42:22 -0300

    added new banner component to apply material custom theme to it

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+})
+export class BannerComponent implements OnInit {
+  @Input()
+  message: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
